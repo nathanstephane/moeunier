dc4f73dd3366d5a4a52393500f1fdd65de8e8b2a
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2023-09-28 20:01:23 +0200

    added lua 5.4 with Sol3 (sol2 v3.0) as binding

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ all: compile link
 
 compile:
 #	g++ -Isrc/include -c main.cpp
-	g++ -Ilibs/sfml/include -c src/*.cpp
+	g++ -std=c++17 -I"./libs/" -Ilibs/sfml/include -c src/*.cpp
 
 link:
 #	g++ main.o -o main -Lsrc/lib -lsfml-graphics -lsfml-window -lsfml-system
-	g++ main.o -o bin/$(PROG) -Llibs/sfml/lib -lsfml-graphics -lsfml-window -lsfml-system
+	g++ main.o -o bin/$(PROG) -Llibs/lua/lib -llua54 -Llibs/sfml/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
 run:
 	.\bin\$(PROG).exe
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <glm/glm.hpp>
 
-int main()
+#include <sol/config.hpp>
+#include <sol/sol.hpp>
+#include <cassert>
+
+int main(int argc, char* argv[])
 {
+    // sol::state lua;
+    // lua.open_libraries(sol::lib::base);
+    
+
+// Create a Lua state
+    sol::state lua;
+
+    // Load a Lua script
+    lua.script("x = 10;");
+
+    // Read a Lua variable
+    int x = lua["x"];
+    std::cout << "The value of x from Lua is: " << x << std::endl;
+
+    // Define a C++ function and make it callable from Lua
+    lua.set_function("add", [](int a, int b) {
+        return a + b;
+    });
+
+    // Call the C++ function from Lua
+    int result = lua.script("return add(5, 3);");
+    std::cout << "The result of adding 5 and 3 in Lua is: " << result << std::endl;
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    glm::vec2 velocity = glm::vec2(5.0,-2.0);
+    
     while (window.isOpen())
     {
         sf::Event event;
