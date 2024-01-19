1df67ceb37599a8b4635ec04f85e5e7e7fc0f294
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2024-01-19 15:14:09 +0100

    ECS structure

---
+++ includes/ECS.hpp
@@ -0,0 +1,34 @@
+#ifndef ECS_H
+#define ECS_H
+
+/*
+    can be refactored to have each class on their
+    separate file
+*/
+
+namespace ECS{
+
+    class Component
+    {
+    
+    };
+    
+    class Entity
+    {
+    
+    };
+    
+    class System
+    {
+    
+    };
+    
+    //The registry class manages and coordates all the game objects
+    class Registry
+    {
+    
+    };
+
+} //end namspace ECS
+
+#endif
